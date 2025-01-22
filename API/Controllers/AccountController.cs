0b31c02aaf14868eac3687d2ea708b245576e6a6
Cassidy Ashe <cassidy.ashe@kbadmin.com>
2025-01-22 13:37:33 -0500

    End of Section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -32,7 +32,8 @@ public class AccountController(DataContext context, ITokenService tokenService, 
         {
             Username = user.UserName,
             Token = tokenService.CreateToken(user),
-            KnownAs = user.KnownAs
+            KnownAs = user.KnownAs,
+            Gender = user.Gender
         };
     }
 
@@ -56,7 +57,8 @@ public class AccountController(DataContext context, ITokenService tokenService, 
             Username = user.UserName,
             KnownAs = user.KnownAs,
             Token = tokenService.CreateToken(user),
-            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
+            PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
+            Gender = user.Gender
         };
     }
 
