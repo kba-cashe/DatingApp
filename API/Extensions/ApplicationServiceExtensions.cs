b567839339f751645bebc0318ed949cc4cba76f2
Cassidy Ashe <cassidy.ashe@kbadmin.com>
2025-01-22 15:54:02 -0500

    End of Section 14

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -20,6 +20,7 @@ public static class ApplicationServiceExtensions
         services.AddOpenApi();
         services.AddScoped<ITokenService, TokenService>();
         services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ILikesRepository, LikesRepository>();
         services.AddScoped<LogUserActivity>();
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
         services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
